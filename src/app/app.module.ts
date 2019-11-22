21caef30a3bce676486d204f00ec36bc87150184
Rodrigo Leon <rodrigoleon@Rodrigos-MacBook-Pro.local>
2019-11-22 15:43:12 -0600

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './components/item/item.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
